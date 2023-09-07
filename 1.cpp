ee5c2e7035501f094c0d53840a45cdfc3dcb4408
Dmitriy Oleynikov <dima2004.com12@gmail.com>
2023-09-07 22:04:48 +0700

    Add file

---
+++ 1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <stdlib.h>
+#include <ctime>
+
+int* genRandArray(int size, int maxValue){
+    int* array = new int [size];
+    array[0] = size;
+    for(int i=1; i<size; i++){
+        int random_int = rand()%maxValue;
+        array[i] = random_int;
+    }
+    
+    return array;
+}
+
+void print(int* array){
+    int count = 0;
+    for(int i=0; i<10; i++){
+        if (array[i] != '\0'){
+            count++;
+        }
+    }
+
+    for(int i=0; i<count; i++){
+        std::cout << array[i];
+        std::cout << "\n";
+    }
+}
+
+int main() {
+    srand(time(0));
+    int size = rand() % 10;
+    int maxValue = 100;
+    int* arr = genRandArray(size, maxValue);
+    print(arr);
+    delete arr;
+
+    return 0;
+}
